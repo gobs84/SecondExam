96629e7e1316c8af11e3fb399b889d5444c87f65
gps <gps9705@gmail.com>
2018-05-10 19:31:05 -0400

    CI-001: Adding Grunfile, and html files

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt){
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    //var config = grunt.file.readJSON('config.json');
+    var config = {}
+    config.buildFolder = grunt.option('build');
+    config.name = grunt.option('config');
+    config.pageOneName = grunt.option('pageOne');
+    config.pageTwoName = grunt.option('pageTwo');
+    config.appName = grunt.option('appName');
+  
+    grunt.registerTask('generateIndex', function(){
+          var config = grunt.file.readJSON(config.name+'.json');
+          grunt.file.copy('index.html', config.buildFolder+'/index.html',{ 
+              process: function(files){
+                  return grunt.template.process(files,
+                  {
+                        data: {
+                          appName:config.appName
+                      }
+                  });
+              }
+          });      
+    });
+    
+    grunt.registerTask('generatep1', function(){
+        var config = grunt.file.readJSON(config.name+'.json');
+        grunt.file.copy('page1.html', config.buildFolder+'/'+config.pageOneName+'.html',{ 
+            process: function(files){
+                return grunt.template.process(files,
+                {
+                      data: {
+                        appName:config.appName
+                    }
+                });
+            }
+        });      
+    });
+
+    grunt.registerTask('generatep1', function(){
+        var config = grunt.file.readJSON(config.name+'.json');
+        grunt.file.copy('page1.html', config.buildFolder+'/'+config.pageOneName+'.html',{ 
+            process: function(files){
+                return grunt.template.process(files,
+                {
+                      data: {
+                        appName:config.appName
+                    }
+                });
+            }
+        });      
+    });
+  };

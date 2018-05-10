e2e1894350d321fe68d65c0794990582d99349da
gps <gps9705@gmail.com>
2018-05-10 19:45:22 -0400

    CI-001: Fixes in Grunfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,9 @@
 module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-concat');
     //var config = grunt.file.readJSON('config.json');
-    var config = {}
-    config.buildFolder = grunt.option('build');
-    config.name = grunt.option('config');
-    config.pageOneName = grunt.option('pageOne');
-    config.pageTwoName = grunt.option('pageTwo');
-    config.appName = grunt.option('appName');
-  
+    var config = grunt.file.readJSON(grunt.option('config'))
+ 
     grunt.registerTask('generateIndex', function(){
-          var config = grunt.file.readJSON(config.name);
           grunt.file.copy('index.html', config.buildFolder+'/index.html',{ 
               process: function(files){
                   return grunt.template.process(files,
@@ -23,7 +17,6 @@ module.exports = function(grunt){
     });
     
     grunt.registerTask('generatep1', function(){
-        var config = grunt.file.readJSON(config.name);
         grunt.file.copy('page1.html', config.buildFolder+'/'+config.pageOneName+'.html',{ 
             process: function(files){
                 return grunt.template.process(files,
@@ -36,9 +29,8 @@ module.exports = function(grunt){
         });      
     });
 
-    grunt.registerTask('generatep1', function(){
-        var config = grunt.file.readJSON(config.name);
-        grunt.file.copy('page1.html', config.buildFolder+'/'+config.pageOneName+'.html',{ 
+    grunt.registerTask('generatep2', function(){
+        grunt.file.copy('page2.html', config.buildFolder+'/'+config.pageTwoName+'.html',{ 
             process: function(files){
                 return grunt.template.process(files,
                 {
@@ -49,4 +41,7 @@ module.exports = function(grunt){
             }
         });      
     });
+
+    grunt.registerTask('build',
+        ['generateIndex','generatep1','generatep2']);
   };

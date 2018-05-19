8f4c760e7ea961e511824e54da1814fb35cf88d1
Ana Paola Montero <anapao.montero@gmail.com>
2018-05-19 01:25:46 -0400

    SE-002: Adding content component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule

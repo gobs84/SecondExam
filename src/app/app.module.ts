1dad77a63296c8136d5b2f44368208c2fca20cd5
Ana Paola Montero <anapao.montero@gmail.com>
2018-05-19 01:16:26 -0400

    SE-001: Adding cards content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
